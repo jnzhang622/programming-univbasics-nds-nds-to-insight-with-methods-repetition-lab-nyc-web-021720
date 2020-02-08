456442a84c5824eb18f934978479010a5e15d27c
John Zhang <jnzhang622@gmail.com>
2020-02-08 05:33:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def total_gross(source)
   total = 0
   director_index = 0
   while director_index < source.size do
-    total += gross_for_director[director_index]
+    total += gross_for_director[source[director_index]]
   end
   total
 end
